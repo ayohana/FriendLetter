d9bd7f2096361da03fec7cfe66a2137d4cae99db
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-08 16:39:16 -0700

    Create views, use views via HomeController

--- FriendLetter.Solution/FriendLetter/Controllers/HomeController.cs
+++ FriendLetter.Solution/FriendLetter/Controllers/HomeController.cs
@@ -16,7 +16,14 @@ namespace FriendLetter.Controllers
     public string Goodbye() { return "Goodbye friend."; }
 
     [Route("/")] // A root path and it's the homepage for our site
-    public string Letter() { return "Our virtual postcard will go here soon!"; }
+    public ActionResult Letter() { return View(); }
+    // ActionResult is a built-in MVC class that handles rendering Views.
+    // View() is a built-in method from Microsoft.AspNetCore.Mvc namespace. It locate views by name:
+      // because views should always reside in a Views directory, View() first locates the Views directory in the production project.
+      // then View() looks for a subdirectory with a name that matches the controller name, i.e. Home (in this case, our Letter() route is in a HomeController so it looks for the subdirectory Home.)
+      // once in the subdirectory, View() then looks for a file that corresponds with the controller's route name (not the route decorator), i.e. Letter (it looks for Letter() -- Letter.cshtml)
+    // NAMING CONVENTION IS VERY IMPORTANT HERE!
+    // NOTE: VIEW() DOES NOT CARE ABOUT THE ROUTE DECORATOR!
 
   }
 }
