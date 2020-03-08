40d0cb2faf8707360238e431b06c50ec012b7d59
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-08 16:18:18 -0700

    Add route decoraters and root path in HomeController.cs

--- FriendLetter.Solution/FriendLetter/Controllers/HomeController.cs
+++ FriendLetter.Solution/FriendLetter/Controllers/HomeController.cs
@@ -7,10 +7,16 @@ namespace FriendLetter.Controllers
   public class HomeController : Controller
   {
 
-    //Hello() method represents a route in our application. Because this method is a route, it will create a special path or pattern in our application. If we were to host this application, we'd have the following route because of the Hello() method in our controller: http://localhost:5000/home/hello (The path is the portion appended to the end of URL: /home/hello)
+    // The route decorater below provides additional info to a route we define. It overrides the default URL path so instead of needing to visit /home/hello, we can just type in /hello.
+    [Route("/hello")]
     public string Hello() { return "Hello friend!"; }
+    //Hello() method represents a route in our application. Because this method is a route, it will create a special path or pattern in our application. If we were to host this application, we'd have the following route because of the Hello() method in our controller: http://localhost:5000/home/hello (The path is the portion appended to the end of URL: /home/hello)
 
+    [Route("/goodbye")]
     public string Goodbye() { return "Goodbye friend."; }
+
+    [Route("/")] // A root path and it's the homepage for our site
+    public string Letter() { return "Our virtual postcard will go here soon!"; }
 
   }
 }
